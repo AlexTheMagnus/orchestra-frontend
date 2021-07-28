b8e2a15b1f599a8feb84a0fc50ce2fa5ef6c15f2
AlexTheMagnus <i72lulem@uco.es>
2021-07-28 14:11:13 +0200

    add: createSoundtrackButton

---
+++ src/components/CreateSoundtrackButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+
+import OrchestraColors from '../constants/OrchestraColors';
+
+const CreateSoundtrackButton = ({ ...props }) => {
+  return (
+    <Button
+      mode="contained"
+      color={OrchestraColors.secondaryColor}
+      dark={true}
+      onPress={() => console.log('Pressed')}
+      style={styles.button}
+      labelStyle={styles.buttonText}
+    >
+      CREATE SOUNDTRACK
+    </Button>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    marginTop: 10
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    paddingVertical: 10
+  }
+});
+
+export default CreateSoundtrackButton;
