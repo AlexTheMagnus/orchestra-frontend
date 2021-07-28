10e6fe0f7404cd33cc9904fa9c144675f6b214e7
AlexTheMagnus <i72lulem@uco.es>
2021-07-28 18:58:53 +0200

    wip:  chooseSoundtrackTitle modal

--- src/screens/MySoundtracksScreen.tsx
+++ src/screens/MySoundtracksScreen.tsx
@@ -1,20 +1,76 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
+import { Dialog, Portal, TextInput, Button } from 'react-native-paper';
 
 import EmptyView from '../components/EmptyView';
 import CreateSoundtrackButton from '../components/CreateSoundtrackButton';
 import SoundtrackItemList from '../components/SoundtrackItemList';
+import OrchestraColors from '../constants/OrchestraColors';
 
 const MySoundtracksScreen = () => {
+  const [visible, setVisible] = React.useState(false);
+  const [newSoundtrackTitle, setNewSoundtrackTitle] = React.useState('');
+
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => {
+    setVisible(false);
+    setNewSoundtrackTitle('');
+  };
+
+  const ChooseSoundtrackTitleModal = () => {
+    return (
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog} style={styles.modal}>
+          <Dialog.Title style={styles.modalTitle}>
+            Choose a title for your soundtrack
+          </Dialog.Title>
+          <Dialog.Content style={styles.textInput}>
+            <TextInput
+              mode="flat"
+              selectionColor={OrchestraColors.primaryColorLightest}
+              underlineColor={OrchestraColors.primaryColorLightest}
+              outlineColor={OrchestraColors.primaryColorLightest}
+              label="NewSoundtrackTitle"
+              value={newSoundtrackTitle}
+              onChangeText={newSoundtrackTitle =>
+                setNewSoundtrackTitle(newSoundtrackTitle)
+              }
+              style={styles.textInput}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              uppercase={false}
+              color={OrchestraColors.primaryColorLightest}
+              labelStyle={styles.cancelButton}
+              onPress={hideDialog}
+            >
+              Cancel
+            </Button>
+            <Button
+              uppercase={false}
+              color={OrchestraColors.primaryColorLightest}
+              labelStyle={styles.nextButton}
+              onPress={hideDialog}
+            >
+              Next
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    );
+  };
+
   const emptyMessage: string =
-    'Touch the button above to create your fisrt soundtrack';
+    'Touch the button above to create your first soundtrack';
 
   const isMySoundtracksEmpty = false;
 
   return (
     <View style={styles.screeenContainer}>
-      <CreateSoundtrackButton />
+      <CreateSoundtrackButton onPress={showDialog} />
+      <ChooseSoundtrackTitleModal />
       {isMySoundtracksEmpty ? (
         <View style={styles.content}>
           <EmptyView icon="mySoundtracks" message={emptyMessage} />
@@ -47,6 +103,27 @@ const styles = StyleSheet.create({
     fontSize: 20,
     lineHeight: 24,
     textAlign: 'center'
+  },
+  modalTitle: {
+    fontSize: 20,
+    lineHeight: 24,
+    textAlign: 'center',
+    color: OrchestraColors.textColor
+  },
+  modal: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: OrchestraColors.primaryColor
+  },
+  textInput: {
+    width: '100%'
+  },
+  nextButton: {
+    color: OrchestraColors.secondaryColor,
+    fontWeight: 'bold'
+  },
+  cancelButton: {
+    color: OrchestraColors.textColorDark
   }
 });
 
