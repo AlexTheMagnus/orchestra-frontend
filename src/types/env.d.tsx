28169a0f7e2038287552689234279a4075307f30
AlexTheMagnus <i72lulem@uco.es>
2021-07-30 15:06:18 +0200

    add: create soundtrack request

--- src/types/env.d.tsx
+++ src/types/env.d.tsx
@@ -1,6 +1,7 @@
 declare module '@env' {
   export const EXPO_CLIENT_ID: string;
   export const ANDROID_CLIENT_ID: string;
+  export const BACKEND_URL: string;
 }
 
 declare module 'react-native-dialog-input';
