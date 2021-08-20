f4ee57331e9c6e19f08f3b5845285f67fc5f22a1
AlexTheMagnus <i72lulem@uco.es>
2021-08-20 21:55:54 +0200

    add: top icons and empty soundtrack view

--- src/components/SoundtrackInfo.tsx
+++ src/components/SoundtrackInfo.tsx
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
     aspectRatio: 2 / 3,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 30
+    marginTop: 50
   }
 });
 
--- src/screens/SoundtrackScreen.tsx
+++ src/screens/SoundtrackScreen.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import SoundtrackInfo from '../components/SoundtrackInfo';
+import { IconButton } from 'react-native-paper';
 
 import { BottomTabParamList } from '../types/types';
+import SoundtrackInfo from '../components/SoundtrackInfo';
+import EmptyView from '../components/EmptyView';
 
 const SoundtrackScreen = ({
   navigation
 }: StackScreenProps<BottomTabParamList, 'Soundtrack'>) => {
+  const emptyMessage: string = 'This soundtrack is currently empty';
+
   return (
-    <SoundtrackInfo
-      bookCover="http://books.google.com/books/content?id=01lAtAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-      soundtrackTitle="El sonido de los Cuervos"
-      bookTitle="Crooked kingdom"
-      author="AlexMagnus"
-    />
+    <View>
+      <IconButton
+        icon="heart-outline"
+        onPress={() => console.log('Corazón')}
+        color="black"
+        size={30}
+        style={styles.favoriteButton}
+      />
+      <IconButton
+        icon="dots-vertical"
+        onPress={() => console.log('Opciones')}
+        color="black"
+        size={30}
+        style={styles.optionsButton}
+      />
+      <SoundtrackInfo
+        bookCover="http://books.google.com/books/content?id=01lAtAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+        soundtrackTitle="El sonido de los Cuervos"
+        bookTitle="Crooked kingdom"
+        author="AlexMagnus"
+      />
+      <View style={styles.emptyViewContainer}>
+        <EmptyView icon="mySoundtracks" message={emptyMessage} />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  favoriteButton: {
+    position: 'absolute',
+    left: 5,
+    top: 20
+  },
+  optionsButton: {
+    position: 'absolute',
+    right: 5,
+    top: 20
+  },
+  emptyViewContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default SoundtrackScreen;
