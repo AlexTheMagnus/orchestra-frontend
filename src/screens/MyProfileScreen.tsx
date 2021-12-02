b47d739bb0d8769dc58526ae818c2aa8eda450d6
AlexTheMagnus <malejandrolule@gmail.com>
2021-12-02 14:07:31 +0100

    improvement: add avatar placeholder

--- src/screens/MyProfileScreen.tsx
+++ src/screens/MyProfileScreen.tsx
@@ -45,7 +45,11 @@ const MyProfileScreen = ({
       <View style={styles.centeredContent}>
         <Avatar.Image
           size={150}
-          source={{ uri: globalState.loggedUser.picture }}
+          source={
+            globalState.loggedUser.picture
+              ? { uri: globalState.loggedUser.picture }
+              : require('../assets/images/avatar-placeholder.png')
+          }
         />
         <Title>{globalState.loggedUser.given_name}</Title>
       </View>
