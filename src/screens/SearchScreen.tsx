ac6aad318aa757b54e9d7bd60f38b0d39f2c0a9d
AlexTheMagnus <malejandrolule@gmail.com>
2021-12-12 13:03:25 +0100

    remove: forgotten console.logs

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -93,7 +93,6 @@ export default function SearchScreen() {
 
         if (foundSoundtracks) {
           soundtrackResults.push(...foundSoundtracks);
-          console.log('soundtrackResults', soundtrackResults);
         }
         i++;
       }
