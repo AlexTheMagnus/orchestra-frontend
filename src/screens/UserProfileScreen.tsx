1addc4f6cdfc615f7d38c3bbed38709107aff006
AlexTheMagnus <malejandrolule@gmail.com>
2021-12-14 21:34:39 +0100

    add: display user info

--- src/screens/UserProfileScreen.tsx
+++ src/screens/UserProfileScreen.tsx
@@ -1,37 +1,69 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Avatar, Title } from 'react-native-paper';
 import { StackScreenProps } from '@react-navigation/stack';
 import { StyleSheet } from 'react-native';
 
 import { getUserSoundtracks } from '../components/utils';
-import { SoundtrackItemParamList, StackParamList } from '../types/types';
+import {
+  SoundtrackItemParamList,
+  StackParamList,
+  UserParamList
+} from '../types/types';
 import { View } from '../components/Themed';
-import AppContext from '../../AppContext';
 import SoundtrackItemList from '../components/SoundtrackItemList';
+import { BACKEND_URL } from '@env';
 
 const UserProfileScreen = ({
-  route,
-  navigation
+  route
 }: StackScreenProps<StackParamList, 'UserProfile'>) => {
   const { userId }: { userId: string } = route.params;
-  const globalState = useContext(AppContext);
+
+  const [userInfo, setUserInfo] = React.useState<UserParamList>({
+    id: '',
+    username: '',
+    avatar: ''
+  });
 
   const [userSoundtracksList, setUserSoundtracksList] = React.useState<
     SoundtrackItemParamList[]
   >([]);
+
+  useEffect(() => {
+    if (userId) {
+      getUserInfo().then(newUserInfo => setUserInfo(newUserInfo));
+      getUserSoundtracks(userId).then(userSoundtracks =>
+        setUserSoundtracksList(userSoundtracks)
+      );
+    }
+  }, []);
 
   const numberOfSoundtracksMessage = (): string =>
     `${userSoundtracksList.length} ${
       userSoundtracksList.length == 1 ? 'soundtrack' : 'soundtracks'
     }`;
 
-  useEffect(() => {
-    if (userId) {
-      getUserSoundtracks(userId).then(userSoundtracks => {
-        setUserSoundtracksList(userSoundtracks);
-      });
+  const getUserInfo = async () => {
+    const response = await fetch(`${BACKEND_URL}/users/${userId}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      const message = `An error has occured loading the user information: Status error ${response.status}`;
+      alert(message);
     }
-  }, []);
+
+    const jsonResponse = await response.json();
+
+    return {
+      id: jsonResponse.user_id,
+      username: jsonResponse.username,
+      avatar: jsonResponse.user_avatar
+    };
+  };
 
   return (
     <View style={styles.container}>
@@ -39,12 +71,12 @@ const UserProfileScreen = ({
         <Avatar.Image
           size={150}
           source={
-            globalState.loggedUser.avatar
-              ? { uri: globalState.loggedUser.avatar }
+            userInfo.avatar
+              ? { uri: userInfo.avatar }
               : require('../assets/images/avatar-placeholder.png')
           }
         />
-        <Title>{globalState.loggedUser.username}</Title>
+        <Title>{userInfo.username}</Title>
       </View>
 
       <Title>{numberOfSoundtracksMessage()}</Title>
