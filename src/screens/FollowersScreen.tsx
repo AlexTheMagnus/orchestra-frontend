a747557547d780d026c88d9c2c6c711d995ac70b
AlexTheMagnus <malejandrolule@gmail.com>
2021-12-27 12:10:16 +0100

    add: header to follower and following screens

--- src/screens/FollowersScreen.tsx
+++ src/screens/FollowersScreen.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Appbar } from 'react-native-paper';
 import { StackScreenProps } from '@react-navigation/stack';
+import { StyleSheet, View } from 'react-native';
 
-import { StackParamList, UserResponse } from '../types/types';
 import { getFollowers } from '../components/utils';
+import { StackParamList, UserResponse } from '../types/types';
+import OrchestraColors from '../constants/OrchestraColors';
 import UserList from '../components/UserList';
 
 const FollowersScreen = ({
+  navigation,
   route
 }: StackScreenProps<StackParamList, 'Followers'>) => {
   const { userId }: { userId: string } = route.params;
@@ -15,7 +19,24 @@ const FollowersScreen = ({
     getFollowers(userId).then(result => setFollowerList(result));
   }, []);
 
-  return <UserList userList={followerList} />;
+  return (
+    <View style={styles.screenContainer}>
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Followers" />
+      </Appbar.Header>
+      <UserList userList={followerList} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1
+  },
+  header: {
+    backgroundColor: OrchestraColors.primaryColorLight
+  }
+});
 
 export default FollowersScreen;
